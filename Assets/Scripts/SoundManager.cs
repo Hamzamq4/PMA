564c366b533d6c9bb3a2bb79f2eef4686deb76dc
Hamzamq4 <92908776+Hamzamq4@users.noreply.github.com>
2022-05-27 01:09:41 +0200

    Singleton
    Plz work

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _musicSource, _effectSource, _thirdSource;
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        _effectSource.PlayOneShot(clip);
+    }
+
+    public void ToggleEffects()
+    {
+        _effectSource.mute = !_effectSource.mute;
+    }
+
+    public void ToggleMusic()
+    {
+        _musicSource.mute = !_musicSource.mute;
+    }
+    public void ToggleThird()
+    {
+        _thirdSource.mute = !_thirdSource.mute;
+    }
+}
+  
